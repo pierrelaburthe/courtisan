e342fd9f0882a0ef507f57dec3671a63c0249b4f
Pierre Laburthe <laburthe35@gmail.com>
2017-08-22 12:52:36 +0200

    animation jquery du feu

---
+++ app/assets/javascripts/fadeout.js
@@ -0,0 +1,13 @@
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(3000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
